a892faafdfb58509730abb712a13f313b19cc185
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-08-31 16:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+let codes = document.querySelectorAll('.code');
+
+
+codes.forEach((code, index)=>{
+    code.addEventListener("input",() =>{
+        if(code.value.length == 1){
+            if(index < codes.length - 1){
+                codes[index+1].focus()
+            }
+        }
+
+    })
+
+    code.addEventListener("keydown",(e)=>{
+
+        if(e.key == "Backspace"){
+            if(index>0 && code.value.length == 0){
+                codes[index-1].focus() 
+            }
+        }
+
+    })
+})
+
+
